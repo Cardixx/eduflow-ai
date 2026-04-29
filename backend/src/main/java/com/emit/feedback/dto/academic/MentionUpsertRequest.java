9db7fde12cc82bd077f08c9ba17e943a350e6d22
Cardixx <hariveloharentsoasarobidy@gmail.com>
2026-04-29 22:28:52 +0300

    deleting

--- backend/src/main/java/com/emit/feedback/dto/academic/MentionUpsertRequest.java
+++
@@ -1,9 +0,0 @@
-package com.emit.feedback.dto.academic;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MentionUpsertRequest(
-        @NotBlank String code,
-        @NotBlank String name
-) {
-}
